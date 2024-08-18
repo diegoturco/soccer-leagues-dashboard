0c52ba522a44970ef2251c4020c57758b69f14f0
Diego Turco <dhturco@gmail.com>
2024-08-18 17:59:26 -0300

    feat: add api route

--- src/app/api/route/route.ts
+++ src/app/api/route/route.ts
@@ -18,16 +18,24 @@
 //   return res.json();
 // }
 
+// export async function GET() {
+//   const leagueCodes = ["eng.1", "esp.1", "ita.1", "ger.1", "fra.1"];
+//   const europeanSeason = new Date().getFullYear() - 1;
+//   const promises = leagueCodes.map((code) =>
+//     fetchLeagueByCode({ code, season: europeanSeason })
+//   );
+
+//   const brazilSeason = new Date().getFullYear();
+//   promises.push(fetchLeagueByCode({ code: "bra.1", season: brazilSeason }));
+
+//   const res = await Promise.all(promises);
+//   return Response.json(res);
+// }
+
 export async function GET() {
-  // const leagueCodes = ["eng.1", "esp.1", "ita.1", "ger.1", "fra.1"];
-  // const europeanSeason = new Date().getFullYear() - 1;
-  // const promises = leagueCodes.map((code) =>
-  //   fetchLeagueByCode({ code, season: europeanSeason })
-  // );
-
-  // const brazilSeason = new Date().getFullYear();
-  // promises.push(fetchLeagueByCode({ code: "bra.1", season: brazilSeason }));
-
-  // const res = await Promise.all(promises);
-  return Response.json({ message: 'hello' });
+  return new Response(JSON.stringify({ message: 'hello' }), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }

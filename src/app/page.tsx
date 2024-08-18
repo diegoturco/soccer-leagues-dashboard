63ce940137c7e0573d51bb2c2442f0536e66150d
Diego Turco <dhturco@gmail.com>
2024-08-18 17:18:05 -0300

    feat: add api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Table from "../components/Table";
 import { BASE_URL } from "./api/config";
 
 export default async function DashboardPage() {
@@ -8,15 +7,15 @@ export default async function DashboardPage() {
   const res = await fetch(url, {
     method: "POST",
   });
-  const data = await res.json();
+  // const data = await res.json();
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-3 h-full xl:h-screen 2xl:h-screen">
-      <Table league={data[0]} />
+      {/* <Table league={data[0]} />
       <Table league={data[1]} />
       <Table league={data[2]} />
       <Table league={data[3]} />
       <Table league={data[4]} />
-      <Table league={data[5]} />
+      <Table league={data[5]} /> */}
     </div>
   );
 }

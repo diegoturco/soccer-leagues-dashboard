ddac069b0351e9f931a698d9c0a6f2a1170ed177
Diego Turco <dhturco@gmail.com>
2024-08-18 17:20:27 -0300

    feat: add api route

--- src/app/api/route/route.ts
+++ src/app/api/route/route.ts
@@ -20,15 +20,15 @@ async function fetchLeagueByCode({
 }
 
 export async function POST() {
-  const leagueCodes = ["eng.1", "esp.1", "ita.1", "ger.1", "fra.1"];
-  const europeanSeason = new Date().getFullYear() - 1;
-  const promises = leagueCodes.map((code) =>
-    fetchLeagueByCode({ code, season: europeanSeason })
-  );
+  // const leagueCodes = ["eng.1", "esp.1", "ita.1", "ger.1", "fra.1"];
+  // const europeanSeason = new Date().getFullYear() - 1;
+  // const promises = leagueCodes.map((code) =>
+  //   fetchLeagueByCode({ code, season: europeanSeason })
+  // );
 
-  const brazilSeason = new Date().getFullYear();
-  promises.push(fetchLeagueByCode({ code: "bra.1", season: brazilSeason }));
+  // const brazilSeason = new Date().getFullYear();
+  // promises.push(fetchLeagueByCode({ code: "bra.1", season: brazilSeason }));
 
-  const res = await Promise.all(promises);
-  return NextResponse.json(res);
+  // const res = await Promise.all(promises);
+  return NextResponse.json({ message: 'hello' });
 }

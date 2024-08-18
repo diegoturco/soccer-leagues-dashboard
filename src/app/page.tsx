4f6a37b757a01f6d394955be8a891844e3c82ed4
Diego Turco <dhturco@gmail.com>
2024-08-18 18:25:30 -0300

    feat: add api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ export default async function DashboardPage() {
   console.log(`BASE_URL: ${BASE_URL}`);
   const url = `${BASE_URL}/api/route`;
 
-  const res = await fetch(url);
-  console.log(res);
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    }
+  });
+  console.log(await res.json());
   
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-3 h-full xl:h-screen 2xl:h-screen">

dad93ba5ccd81de764cf95821a2e78e9fdb0edd1
Diego Turco <dhturco@gmail.com>
2024-08-18 17:27:57 -0300

    feat: add api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ export default async function DashboardPage() {
   console.log(`BASE_URL: ${BASE_URL}`);
   const url = `${BASE_URL}/api/route`;
 
-  // const res = await fetch(url);
-  // const data = await res.json();
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-3 h-full xl:h-screen 2xl:h-screen">
       {/* <Table league={data[0]} />

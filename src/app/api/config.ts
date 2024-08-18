1fd7ab71d3207650974eef23792c4374fe94e5a5
Diego Turco <dhturco@gmail.com>
2024-08-18 18:38:02 -0300

    feat: add api route

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+export const BASE_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";

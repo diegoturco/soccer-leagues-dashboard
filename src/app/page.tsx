32e98a44ae16a3434ae8ecee4627d2f7e2a4c54a
Diego Turco <dhturco@gmail.com>
2024-08-18 16:24:31 -0300

    feat: add api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,21 @@
-import { League } from "@/interfaces";
+import { headers } from "next/headers";
 import Table from "../components/Table";
-
-interface LeagueProps {
-  code: string;
-  season: number;
-}
-
-async function fetchLeagueByCode({
-  code,
-  season,
-}: LeagueProps): Promise<League> {
-  const res = await fetch(
-    `https://site.web.api.espn.com/apis/v2/sports/soccer/${code}/standings?season=${season}`,
-    { next: { revalidate: 5 } }
-  );
-  
-  return res.json();
-}
+import { BASE_URL } from "./api/config";
 
 export default async function DashboardPage() {
-  const leagueCodes = ["eng.1", "esp.1", "ita.1", "ger.1", "fra.1"];
-  const europeanSeason = new Date().getFullYear() - 1;
-  const promises = leagueCodes.map((code) =>
-    fetchLeagueByCode({ code, season: europeanSeason })
-  );
-
-  const brazilSeason = new Date().getFullYear();
-  promises.push(fetchLeagueByCode({ code: "bra.1", season: brazilSeason }));
-
-  const res = await Promise.all(promises);
-
+  const res = await fetch(`${BASE_URL}/api/route`, {
+    headers: headers(),
+    method: 'POST'
+  }); 
+  const data = await res.json();
   return (
     <div className="grid grid-cols-1 xl:grid-cols-3 2xl:grid-cols-3 h-full xl:h-screen 2xl:h-screen">
-      <Table league={res[0]} />
-      <Table league={res[1]} />
-      <Table league={res[2]} />
-      <Table league={res[3]} />
-      <Table league={res[4]} />
-      <Table league={res[5]} />
+      <Table league={data[0]} />
+      <Table league={data[1]} />
+      <Table league={data[2]} />
+      <Table league={data[3]} />
+      <Table league={data[4]} />
+      <Table league={data[5]} />
     </div>
   );
 }
